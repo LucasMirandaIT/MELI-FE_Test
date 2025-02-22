01c34e1ba407389e1abb0fb083e63949935f0482
Lucas Miranda <lucasmiranda@Lucass-MacBook-Pro.local>
2025-02-22 10:49:22 -0300

    fix(product-details): add N/A to products without description

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -76,7 +76,8 @@ export default function ItemById() {
         <hr className="product__divider" />
         <div className="product__description">
           <h2 className="product__description__title">Descripción</h2>
-          <p className="product__description__content">{product.description}</p>
+          <p className="product__description__content">{product.description || 'N/A'}</p>
+          {/* <p className="product__description__content">{product.description ? product.description : 'N/A'}</p> */}
         </div>
       </div>
 
